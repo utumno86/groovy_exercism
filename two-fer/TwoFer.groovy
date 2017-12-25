0464a3433a161098a35f92b16413c4c39edd0e29
mnash <utumno86@gmail.com>
2017-12-24 23:22:12 -0500

    revised groovy two-fer

--- two-fer/TwoFer.groovy
+++ two-fer/TwoFer.groovy
@@ -1,9 +1,5 @@
 class TwoFer {
   static String twoFer(String name) {
-    if (name?.trim() == '' || name == null){
-      return "One for you, one for me."
-    } else {
-      return "One for ${name}, one for me."
-    }
+    "One for ${name ?: 'you'}, one for me."
   }
 }
