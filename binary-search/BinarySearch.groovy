6f07a0c683cd3fee7f434fd0bdfe0b36ec8b0102
Michael Nash <michael@spatialnetworks.com>
2018-01-18 16:58:42 -0500

    groovy binary search work in progress

--- binary-search/BinarySearch.groovy
+++ binary-search/BinarySearch.groovy
@@ -8,7 +8,31 @@ class BinarySearch {
     }
 
     int indexOf(item) {
-        throw new UnsupportedOperationException('indexOf method not implemented')
-    }
+        if (data == []) { return -1 }
+        if (!data.contains(item)) { return -1 }
+        def index = -2
+        int searchIndex = data.size()/2
+        while (index == -2){
+            println data
+            println searchIndex
+            switch(data[searchIndex]){
+                case { it > item }:
+                    println "greater"
+                    searchIndex -= (searchIndex/2)
+                    break
+                case { it < item }:
+                    println "lesser"
+                    searchIndex += ((data.size() - searchIndex)/2)
+                    break
+                case { it == item }:
+                    println "equals"
+                    index = searchIndex
+                    break
+                default:
+                    index = -1
+            }
 
+        }
+        return index
+    }
 }
