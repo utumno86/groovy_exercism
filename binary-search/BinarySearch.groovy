85ddbca81bb4167620bbfecb68a0147efd96f789
Michael Nash <michael@spatialnetworks.com>
2018-01-31 11:24:51 -0500

    completed exercism groovy

--- binary-search/BinarySearch.groovy
+++ binary-search/BinarySearch.groovy
@@ -13,27 +13,27 @@ class BinarySearch {
         def index = -2
         int searchIndex = data.size()/2
         def previousSearchIndex
+        def previousSearchIndexs = []
         while (index == -2){
-            println data
-            println searchIndex
             def currentValue = data[searchIndex]
             previousSearchIndex = searchIndex
+            previousSearchIndexs.push(searchIndex)
             if (currentValue < item){
-                println "greater"
-                println currentValue
                 searchIndex += ((data.size() - searchIndex)/2)
-                while(searchIndex == previousSearchIndex){
+                while(previousSearchIndexs.contains(searchIndex)){
                     searchIndex ++
                 }
             } else if (currentValue > item) {
-                println "lesser"
-                println currentValue
-                searchIndex -= (searchIndex/2)
-                while(searchIndex == previousSearchIndex){
+                searchIndex = (searchIndex/2)
+                previousSearchIndex = searchIndex
+                previousSearchIndexs.push(searchIndex)
+                while(previousSearchIndexs.contains(searchIndex) && searchIndex >= 0){
                     searchIndex --
                 }
+                while (searchIndex < 0 || previousSearchIndexs.contains(searchIndex)){
+                    searchIndex ++
+                }
             } else if(currentValue == item){
-                println "equals"
                 index = searchIndex
             } else {
                 index = -1
