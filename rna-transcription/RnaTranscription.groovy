25e642a9e892c663f718738432a23fdb747a4fcd
Michael Nash <michael@spatialnetworks.com>
2018-02-13 14:19:36 -0500

    updated groovy rna transcription

--- rna-transcription/RnaTranscription.groovy
+++ rna-transcription/RnaTranscription.groovy
@@ -1,10 +1,10 @@
 class RnaTranscription{
 
   def dnaMap = [
-    "C":"G",
-    "G":"C",
-    "T":"A",
-    "A":"U"
+    C:'G',
+    G:'C',
+    T:'A',
+    A:'U',
   ].withDefault{k -> throw new IllegalArgumentException("$k is not a valid Dna strand") }
 
   def ofDNA(input){
