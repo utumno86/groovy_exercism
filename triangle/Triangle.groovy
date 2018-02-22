c98cb00bbcea455660c6aa7c4c1c888740726218
Michael Nash <michael@spatialnetworks.com>
2018-02-22 14:33:09 -0500

    updated groovy triangle

--- triangle/Triangle.groovy
+++ triangle/Triangle.groovy
@@ -1,23 +1,17 @@
 class Triangle {
+	static boolean isEquilateral(Number a, Number b, Number c) {
+		a == b && b == c && a > 0
+	}
 
-    static boolean isEquilateral(Number a, Number b, Number c) {
-      a == b && b == c && a > 0 && passesTriangleEquality(a, b, c)
-    }
+	static boolean isIsosceles(Number a, Number b, Number c) {
+		(a == b || a == c || b == c) && isTriangle(a, b, c)
+	}
 
-    static boolean isIsosceles(Number a, Number b, Number c) {
-      (a == b || a == c || b == c) && allSidesNonZero(a, b, c) && passesTriangleEquality(a, b, c)
-    }
+	static boolean isScalene(Number a, Number b, Number c) {
+		[a, b, c].unique().size() == 3 && isTriangle(a, b, c)
+	}
 
-    static boolean isScalene(Number a, Number b, Number c) {
-      [a, b, c].unique().size() == 3 && allSidesNonZero(a, b, c) && passesTriangleEquality(a, b, c)
-    }
-
-    public static passesTriangleEquality(Number a, Number b, Number c){
-      a + b + c >= 2 * [a, b, c].max()
-    }
-
-    public static allSidesNonZero(Number a, Number b, Number c){
-      ![a, b, c].any { it <= 0 }
-    }
-
+	public static isTriangle(Number a, Number b, Number c) {
+		a + b + c >= 2 * [a, b, c].max() && [a, b, c].every { it >= 0 }
+	}
 }
