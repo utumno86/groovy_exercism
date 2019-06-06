e66eb7dc21750c04bc34fda2d7058fde1687ec4f
Michael Nash <michael@spatialnetworks.com>
2019-06-06 13:40:22 -0400

    updated rotational cipher

--- rotational-cipher/src/main/groovy/RotationalCipher.groovy
+++ rotational-cipher/src/main/groovy/RotationalCipher.groovy
@@ -1,26 +1,22 @@
 class RotationalCipher {
-    private int key
+    private final int key
 
     RotationalCipher(int key) {
         this.key = key
     }
 
-    public String rotate(String cipherText) {
-        def ciphered_string = ''
-        cipherText.each{ c ->
+    String rotate(String cipherText) {
+        cipherText.collect{ c ->
             def character = c as Character
             def ascii = character as Integer
-            def new_char
+            def new_char = ascii
             if (character.isLetter()){
                 new_char = ascii + this.key
                 if ((character.isLowerCase() && new_char > 122) || (character.isUpperCase() && new_char > 90)) {
                     new_char = new_char - 26
                 }
-            } else {
-                new_char = ascii
             }
-            ciphered_string += new_char as Character
-        }
-        ciphered_string
+            new_char as Character
+        }.join('')
     }
 }
